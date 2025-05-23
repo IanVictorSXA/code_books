48f9606efa0294e60ab6e96989044c7eccc3535d
IanVictorSXA <91403350+IanVictorSXA@users.noreply.github.com>
2025-05-22 21:00:02 -0500

    exercise0

---
+++ exercises_for_programmers_Brian_Hogan/exercise0_tip_calculator/exercise0.cpp
@@ -0,0 +1,23 @@
+// Exercise 0: tip calculator
+// prompt bill amount and tip rate
+// output tip and total amount (tip + bill)
+
+import std;
+
+using namespace std;
+
+int main()
+{
+	string bill_str, tip_rate_str; // to store input values in string form
+	double bill, tip_rate; // to store input values in int form
+	cout << "Bill amount: ";
+	cin >> bill_str;
+	cout << "Tip rate: ";
+	cin >> tip_rate_str;
+	
+	//bill = stod(bill_str);
+	//tip_rate = stod(tip_rate_str)/100;
+
+	//cout << "Tip: " << bill * tip_rate << endl;
+	//cout << "Total: " << bill * (1 + tip_rate) << endl;
+}
